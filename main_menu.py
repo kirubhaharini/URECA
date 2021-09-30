f1a14136929df82e82c8e26c630bd8e4fcc50b88
kirubhaharini <kirubhah@gmail.com>
2021-09-30 14:30:10 +0800

    works

--- main_menu.py
+++ main_menu.py
@@ -4,19 +4,18 @@ import school
 state = st.session_state 
 
 state.query_params = st.experimental_get_query_params()
-st.write(state.query_params)
+# st.write(state.query_params)
 
-#state.school_change = 'None' #changing url directly
-state.school = 'SchoolSelection'
+state.school = 'SchoolSelection' #default url query param for selection
 
 if 'school' in state.query_params.keys(): #if url is changed directly
     state.school = state.query_params["school"][0]
 
-school_list = ['Select','NCHS','DHS']
+school_list = ['Select','NCHS','DSS']
 state.ph = st.empty()
 state.dropdownMenu = state.ph.selectbox('school',school_list,index = 0,key='page')
 
-if (state.dropdownMenu != 'Select'): #or (state.school!='SchoolSelection'):
+if (state.dropdownMenu != 'Select'):
     state.school = state.dropdownMenu
     state.ph.empty()
     school.school(state)
